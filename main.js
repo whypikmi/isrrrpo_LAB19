ff297db9db7373db90f0e2a8077b87c6edc6f17b
ElizavetaTelyatnikova <ttelatnikova1@yandex.ru>
2026-03-13 15:04:07 +0300

    Step 7: arrays in JavaScript

--- main.js
+++ main.js
@@ -67,7 +67,6 @@ for (let i = 1; i <= 4; i++) {
     }
     console.log(c);
 }
-*/
 
 function sum(a, b) {
     return a + b;
@@ -95,7 +94,89 @@ function greet(name = "Гость") {
 greet();
 greet("Анастасия");
 
-function calculateDiscount(price,sale=10){
-    return price- (price*sale/100);
+function calculateDiscount(price, sale = 10) {
+    return price - (price * sale / 100);
 }
-console.log(calculateDiscount(1000,50));
+console.log(calculateDiscount(1000, 50));
+
+const add = function (a, b) {
+    return a + b;
+};
+console.log(add(2, 3));
+
+function makeCounter() {
+    let count = 0;
+    return function () {
+        count++;
+        return count;
+    };
+}
+const counter = makeCounter();
+console.log(counter());
+console.log(counter());
+console.log(counter());
+
+function sumFunc(a, b) {
+    return a + b;
+}
+const sumFunc2 = (a, b) => a + b;
+const double = x => x * 2;
+const sumFunc3 = (a, b) => a + b;
+const sayGreeting = () => console.log("Hello");
+const square = x => x * x;
+const calculate = (a, b) => {
+    let result = a + b;
+    return result * 2;
+}
+
+console.log(fruits.includes("яблоко"));
+console.log(fruits.includes("манго"));
+*/
+
+let numbersArr = [1, 2, 3, 4, 5];
+console.log(numbersArr);
+
+console.log(numbersArr[0]);
+console.log(numbersArr[1]);
+
+let colors = ["red", "orange", "pink"];
+console.log(colors[0]);
+console.log(colors[2]);
+colors[1] = "green";
+console.log(colors);
+
+console.log(numbersArr.length);
+numbersArr.push(10);
+console.log(numbersArr);
+numbersArr.pop();
+console.log(numbersArr);
+
+let students = [];
+students.push("Liza");
+students.push("Vanya");
+students.push("Nastya");
+console.log(students);
+students.pop();
+console.log(students);
+
+let numbers2 = [10, 20, 30];
+for (let i = 0; i < numbers2.length; i++) {
+    console.log(numbers2[i]);
+}
+for (let value of numbers2) {
+    console.log(value);
+}
+let mixedArray = [1, "text", true, 3.14];
+console.log(mixedArray);
+console.log(numbersArr.indexOf(1));
+console.log(numbersArr.indexOf(2));
+
+let city = ["Volgograd","Moscow","Perm","Anapa"];
+let town = "Moscow";
+if(city.includes(town)){
+    let ind = city.indexOf(town);
+    console.log(`Город ${town} найден на позиции ${ind}`);
+}
+else{
+    console.log("Нет заданного города");
+}
